47a56f7c860320ff413835975baa035cfb51be5f
ReyADayer <reyaddayer@gmail.com>
2016-12-04 13:00:12 +0900

    :seedling: first commit

---
+++ src/main/java/TutorialPlugin.java
@@ -0,0 +1,13 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TutorialPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO ここに、プラグインが有効化された時の処理を実装してください。
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO ここに、プラグインが無効化された時の処理を実装してください。
+    }
+}
